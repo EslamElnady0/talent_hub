365952280fcb8c5087f1a8d0b83b0265c0704284
MoaazAbdelaziz <moaazwork1@gmail.com>
2024-09-16 17:22:17 +0300

    -Added splash router

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:talent_hub/features/splash/presentation/views/splash_view.dart';
 
 import '../../features/home/presentation/home_view.dart';
 import '../../features/onboarding/presentation/views/onboarding_view.dart';
 
 class AppRouter {
   static const String onboarding = '/onboarding';
+  static const String splash = '/';
   static const String home = '/home';
 
   static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
@@ -13,6 +15,8 @@ class AppRouter {
         return MaterialPageRoute(builder: (context) => const OnBoardingView());
       case home:
         return MaterialPageRoute(builder: (context) => const HomeView());
+      case splash:
+        return MaterialPageRoute(builder: (context) => const SplashView());
       default:
         return null;
     }
