2d894404463d17c16199ee0d392f2716e0e22a64
MoaazAbdelaziz <moaazwork1@gmail.com>
2024-09-16 17:22:30 +0300

    -Created splash view and animations

---
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -0,0 +1,36 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:talent_hub/core/helpers/app_assets.dart';
+import 'package:talent_hub/core/helpers/spacing.dart';
+import 'package:talent_hub/core/theme/app_text_styles.dart';
+import 'package:talent_hub/features/onboarding/presentation/views/onboarding_view.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: AnimatedSplashScreen(
+          splash: Column(
+            children: [
+              Image.asset(AppAssets.appLogo),
+              hGap(7),
+              Text(
+                'TalentHub',
+                style: AppTextStyles.font20DarkW600.copyWith(
+                  fontFamily: 'AzeretMono',
+                  fontSize: 21,
+                ),
+              ),
+            ],
+          ),
+          nextScreen: const OnBoardingView(),
+          duration: 2500,
+          splashTransition: SplashTransition.scaleTransition,
+        ),
+      ),
+    );
+  }
+}
