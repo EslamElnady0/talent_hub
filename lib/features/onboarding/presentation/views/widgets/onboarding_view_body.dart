7b99c28350ca0cdeb78ac6893326b9d4b0ba6bce
MoaazAbdelaziz <moaazwork1@gmail.com>
2024-09-16 22:56:19 +0300

    -Add navigation to register view -Add navigation to login view

--- lib/features/onboarding/presentation/views/widgets/onboarding_view_body.dart
+++ lib/features/onboarding/presentation/views/widgets/onboarding_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:talent_hub/core/helpers/extensions.dart';
 import 'package:talent_hub/core/helpers/spacing.dart';
+import 'package:talent_hub/core/routes/app_router.dart';
 import 'package:talent_hub/core/theme/app_colors.dart';
 import 'package:talent_hub/core/theme/app_text_styles.dart';
 import 'package:talent_hub/features/onboarding/presentation/views/widgets/custom_button.dart';
@@ -38,13 +40,17 @@ class OnboardingViewBody extends StatelessWidget {
               vGap(40),
               CustomButton(
                 text: 'LOGIN',
-                onPressed: () {},
+                onPressed: () {
+                  context.pushNamed(AppRouter.login);
+                },
                 textColor: AppColors.white,
               ),
               vGap(8),
               CustomButton(
                 text: 'CREATE ACCOUNT',
-                onPressed: () {},
+                onPressed: () {
+                  context.pushNamed(AppRouter.register);
+                },
                 backgroundColor: const Color(0xFFEEEEEE),
               ),
             ],
