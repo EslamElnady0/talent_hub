7852ce4b03b83fc724150d027a6d56b65c262aee
MoaazAbdelaziz <moaazwork1@gmail.com>
2024-09-17 19:49:48 +0300

    -Finished ui of login view -Added route to forgot password view

---
+++ lib/features/auth/presentation/views/login_view/widgets/login_view_body.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:talent_hub/core/helpers/extensions.dart';
+import 'package:talent_hub/core/helpers/spacing.dart';
+import 'package:talent_hub/core/routes/app_router.dart';
+import 'package:talent_hub/core/theme/app_colors.dart';
+import 'package:talent_hub/features/auth/presentation/views/login_view/widgets/login_header.dart';
+import 'package:talent_hub/features/auth/presentation/views/register_view/widgets/custom_text_form_field.dart';
+import 'package:talent_hub/features/onboarding/presentation/views/widgets/custom_button.dart';
+
+class LoginViewBody extends StatelessWidget {
+  const LoginViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const LoginHeader(),
+          vGap(20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                CustomTextFormField(
+                  labelText: 'Email',
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    return null;
+                  },
+                ),
+                vGap(16),
+                CustomTextFormField(
+                  labelText: 'Password',
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  validator: (value) {
+                    return null;
+                  },
+                ),
+                vGap(4),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        context.pushNamed(AppRouter.forgotPassword);
+                      },
+                      child: const Text(
+                        'Forgot Password?',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: AppColors.grey,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          vGap(20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: CustomButton(
+                  text: 'SIGN IN',
+                  onPressed: () {},
+                  textColor: AppColors.white,
+                  backgroundColor: AppColors.black,
+                ),
+              ),
+              vGap(20),
+              const Text(
+                'OR',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: AppColors.grey,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              vGap(16),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: CustomButton(
+                  text: 'SIGN UP',
+                  onPressed: () {
+                    context.pushNamed(AppRouter.register);
+                  },
+                  textColor: AppColors.black,
+                  backgroundColor: AppColors.smoke,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
