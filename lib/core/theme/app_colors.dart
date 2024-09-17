c74a3786c1d94401214c28a970edfbf31358cb8f
MoaazAbdelaziz <moaazwork1@gmail.com>
2024-09-17 15:32:47 +0300

    -Added smoke color and text form filed color

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -5,6 +5,10 @@ class AppColors {
 
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
-  
+
   static const Color primaryColor = Color(0xFFFF5050);
+
+  static const Color smoke = Color(0xFFEEEEEE);
+
+  static const Color fieldColor = Color(0xFFA8A8A8);
 }
