962d7dc60bd090da385b896b7233de665b06777e
Your Name <Abdulrahman956>
2024-09-24 17:53:34 +0300

    add routes

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -6,12 +6,15 @@ import 'package:talent_hub/features/chat/presentation/views/chat_inbox_view.dart
 import 'package:talent_hub/features/auth/presentation/views/forgot_pass_view/forgot_pass_view.dart';
 import 'package:talent_hub/features/auth/presentation/views/login_view/login_view.dart';
 import 'package:talent_hub/features/auth/presentation/views/register_view/register_view.dart';
+import 'package:talent_hub/features/player/presentation/views/player_view.dart';
+import 'package:talent_hub/features/player/presentation/views/widgets/post/create_post_view.dart';
 import 'package:talent_hub/features/scout/presentation/views/scout_view.dart';
 
 import 'package:talent_hub/features/splash/presentation/views/splash_view.dart';
 
 import '../../features/home/presentation/home_view.dart';
 import '../../features/onboarding/presentation/views/onboarding_view.dart';
+import '../../features/player/presentation/views/player_profile_view.dart';
 
 class AppRouter {
   static const String onboarding = '/onboarding';
@@ -22,6 +25,9 @@ class AppRouter {
   static const String chatDetails = '/chatDetails';
 
   static const String scout = '/scout';
+  static const String player = '/player';
+  static const String playerProfile = '/playerProfile';
+  static const String post = '/post';
 
   static const String login = '/login';
   static const String register = '/register';
@@ -36,6 +42,15 @@ class AppRouter {
 
       case scout:
         return MaterialPageRoute(builder: (context) => const ScoutView());
+
+      case player:
+        return MaterialPageRoute(builder: (context) => const PlayerView());
+
+      case playerProfile:
+        return MaterialPageRoute(builder: (context) => const PlayerProfileView());
+
+      case post:
+        return MaterialPageRoute(builder: (context) => const CreatePostView());
 
       case splash:
         return MaterialPageRoute(builder: (context) => const SplashView());
