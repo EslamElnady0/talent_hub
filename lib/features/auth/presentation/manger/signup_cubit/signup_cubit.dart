1704a9641eeb5d715c03e6847e6ed7e1376e6397
MoaazAbdelaziz <moaazwork1@gmail.com>
2024-09-25 02:39:22 +0300

    -Edited the code of signup -Added the upload function -Added pick image function

--- lib/features/auth/presentation/manger/signup_cubit/signup_cubit.dart
+++ lib/features/auth/presentation/manger/signup_cubit/signup_cubit.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+import 'package:path/path.dart' as path;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:talent_hub/core/helpers/app_assets.dart';
+import 'package:talent_hub/core/helpers/upload_file_to_firebase_storage.dart';
 import 'package:talent_hub/features/auth/data/repo/auth_repo.dart';
 import 'package:talent_hub/features/auth/presentation/manger/signup_cubit/signup_states.dart';
 
@@ -11,11 +16,40 @@ class SignupCubit extends Cubit<SignupStates> {
   var phoneController = TextEditingController();
   var ageController = TextEditingController();
   var positionController = TextEditingController();
+  File? imageFile;
 
   final AuthRepo _authRepo;
 
+  void pickImage() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      imageFile = File(pickedImage.path);
+      emit(SignupImagePickedState());
+    }
+  }
+
+  Future<String?> uploadImage(File image) async {
+    try {
+      return await uploadFileToFirebaseStorage(
+          path:
+              'users/${DateTime.now().microsecondsSinceEpoch}/${path.basename(image.path)}',
+          file: image);
+    } catch (e) {
+      emit(SignupErrorState(e.toString()));
+      return null;
+    }
+  }
+
   void signup() async {
     emit(SignupLoadingState());
+    String? imageUrl;
+    if (imageFile != null) {
+      imageUrl = await uploadImage(imageFile!);
+      if (imageUrl == null) return;
+    } else {
+      imageUrl = AppAssets.player;
+    }
     var result = await _authRepo.signUp(
       emailController.text,
       passwordController.text,
@@ -23,6 +57,7 @@ class SignupCubit extends Cubit<SignupStates> {
       phoneController.text,
       ageController.text,
       positionController.text,
+      imageUrl,
     );
     result.fold((failure) {
       emit(SignupErrorState(failure.message));
@@ -39,6 +74,7 @@ class SignupCubit extends Cubit<SignupStates> {
     phoneController.dispose();
     ageController.dispose();
     positionController.dispose();
+    imageFile = null;
     return super.close();
   }
 }
