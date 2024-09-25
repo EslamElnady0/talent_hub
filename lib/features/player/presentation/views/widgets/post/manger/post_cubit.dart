f2a2fb7e66f5d14599e9b5ac53bd10c1705eb23e
Your Name <Abdulrahman956>
2024-09-25 15:32:17 +0300

    create post states and cubit

---
+++ lib/features/player/presentation/views/widgets/post/manger/post_cubit.dart
@@ -0,0 +1,71 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:talent_hub/features/player/presentation/model/player_model.dart';
+import 'package:talent_hub/features/player/presentation/views/widgets/post/manger/post_states.dart';
+
+import '../model/posts_model.dart';
+
+class PostsCubit extends Cubit<PostState> {
+  PostsCubit() : super(CreatePostInitialState());
+
+  static PostsCubit get(context) => BlocProvider.of(context);
+  late Player playerModel;
+
+  void createPost({
+    required String uId,
+    required String description,
+    required String postVideo,
+  }) {
+    emit(CreatePostLoadingState());
+    Post postModel = Post(
+      playerId: playerModel.uId,
+      createdAt: DateTime.now(),
+      uId: uId,
+      description: description,
+      videoUrl: postVideo,
+      comments: [],
+      likes: [],
+    );
+    FirebaseFirestore.instance
+        .collection('posts')
+        .add(postModel.toMap())
+        .then((value) {
+      emit(CreatePostSuccessState());
+    }).catchError((error) {
+      emit(CreatePostErrorState(error.toString()));
+    });
+  }
+  List<Post> postsList = [];
+  void getPosts()
+  {
+    emit(GetPostsLoadingState());
+    FirebaseFirestore.instance
+        .collection('posts')
+        .get()
+        .then((value)
+    {
+      for (var element in value.docs) {
+        postsList.add(Post.fromMap(element.data()));
+      }
+      emit(GetPostsSuccessState());
+    })
+        .catchError((error){
+      emit(GetPostsErrorState(error.toString()));
+    });
+  }
+
+  void likePost(String postId) {
+    FirebaseFirestore.instance
+        .collection('posts')
+        .doc(postId)
+        .collection('likes')
+        .doc(playerModel.uId)
+        .set({
+      'like': true,
+    }).then((value) {
+      emit(LikePostSuccessState());
+    }).catchError((error) {
+      emit(LikePostErrorState(error.toString()));
+    });
+  }
+}
---
+++ lib/features/player/presentation/views/widgets/post/manger/post_states.dart
@@ -0,0 +1,29 @@
+abstract class PostState {}
+
+class CreatePostInitialState extends PostState {}
+
+class CreatePostLoadingState extends PostState {}
+
+class CreatePostSuccessState extends PostState {}
+
+class CreatePostErrorState extends PostState {
+  final String message;
+
+  CreatePostErrorState(this.message);
+}
+
+class GetPostsLoadingState extends PostState {}
+
+class GetPostsSuccessState extends PostState {}
+
+class GetPostsErrorState extends PostState {
+  final String error;
+
+  GetPostsErrorState(this.error);
+}
+class LikePostSuccessState extends PostState {}
+class LikePostErrorState extends PostState
+{
+  final String error;
+  LikePostErrorState(this.error);
+}
