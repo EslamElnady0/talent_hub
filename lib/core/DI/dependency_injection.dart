a20ec9a2d93d2698f51de8671c6e6e1dfe118e2f
EslamElnady0 <eslamelnady254@gmail.com>
2024-09-25 12:34:17 +0300

    added send message cubits to di

--- lib/core/DI/dependency_injection.dart
+++ lib/core/DI/dependency_injection.dart
@@ -4,6 +4,10 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:talent_hub/features/auth/data/repo/auth_repo.dart';
 import 'package:talent_hub/features/auth/data/repo/user_repo.dart';
+import 'package:talent_hub/features/chat/data/repo/send_message_repo.dart';
+import 'package:talent_hub/features/chat/presentation/view%20models/send%20file%20cubit/send_file_cubit.dart';
+import 'package:talent_hub/features/chat/presentation/view%20models/send%20record%20cubit/send_record_cubit.dart';
+import 'package:talent_hub/features/chat/presentation/view%20models/send%20text%20cubit/send_text_cubit.dart';
 
 GetIt getIt = GetIt.instance;
 
@@ -14,4 +18,16 @@ void setupServiceLocator() {
   getIt.registerSingleton<UserRepo>(
       UserRepoImpl(getIt.get<FirebaseFirestore>()));
   getIt.registerSingleton<AuthRepo>(AuthRepoImpl(getIt.get<UserRepo>()));
+  getIt.registerLazySingleton<SendMessageRepo>(() => SendMessageRepo(
+      auth: getIt.get<FirebaseAuth>(),
+      firestore: getIt.get<FirebaseFirestore>(),
+      storage: getIt.get<FirebaseStorage>()));
+
+  /////////////////////// chat cubits /////////////////////
+  getIt.registerFactory<SendFileCubit>(
+      () => SendFileCubit(getIt.get<SendMessageRepo>()));
+  getIt.registerFactory<SendRecordCubit>(
+      () => SendRecordCubit(getIt.get<SendMessageRepo>()));
+  getIt.registerFactory<SendTextCubit>(
+      () => SendTextCubit(getIt.get<SendMessageRepo>()));
 }
