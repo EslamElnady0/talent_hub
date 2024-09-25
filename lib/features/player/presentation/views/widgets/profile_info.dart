f629a4084612131a73598109cbe8d60c4767b867
Your Name <Abdulrahman956>
2024-09-25 17:05:01 +0300

    add cubit to player info body

--- lib/features/player/presentation/views/widgets/profile_info.dart
+++ lib/features/player/presentation/views/widgets/profile_info.dart
@@ -1,48 +1,55 @@
 import 'package:flutter/material.dart';
-
-import '../../../../../core/helpers/app_assets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../../core/helpers/spacing.dart';
 import '../../../../../core/theme/app_colors.dart';
+import '../../manger/player_cubit.dart';
+import '../../manger/player_state.dart';
 
 class ProfileInfo extends StatelessWidget {
   const ProfileInfo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Stack(
-          alignment: Alignment.bottomRight,
+    return BlocConsumer<PlayerCubit, PlayerState>(
+      listener: (BuildContext context, state) {},
+      builder: (BuildContext context, state) {
+        var userModel = PlayerCubit.get(context).playerModel;
+        return Column(
           children: [
-            CircleAvatar(
-              backgroundColor: AppColors.primaryColor,
-              radius: 80,
-              child: CircleAvatar(
-                radius: 78,
-                backgroundImage: AssetImage(AppAssets.player),
-              ),
-            ),
-            CircleAvatar(
-              radius: 19,
-              backgroundColor: AppColors.white,
-              child: IconButton(
-                icon: const Icon(
-                  Icons.edit,
-                  color: AppColors.primaryColor,
-                  size: 19,
+            Stack(
+              alignment: Alignment.bottomRight,
+              children: [
+                CircleAvatar(
+                  backgroundColor: AppColors.primaryColor,
+                  radius: 80,
+                  child: CircleAvatar(
+                    radius: 78,
+                    backgroundImage: AssetImage(userModel.image),
+                  ),
                 ),
-                onPressed: () {},
-              ),
+                CircleAvatar(
+                  radius: 19,
+                  backgroundColor: AppColors.white,
+                  child: IconButton(
+                    icon: const Icon(
+                      Icons.edit,
+                      color: AppColors.primaryColor,
+                      size: 19,
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
             ),
+            vGap(15),
+             Text(
+              userModel.name,
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+             Text(userModel.email, style: const TextStyle(fontSize: 20)),
           ],
-        ),
-        vGap(15),
-        const Text(
-          "Abdelrahman",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-        const Text("abdelrahman@gmail.com", style: TextStyle(fontSize: 20)),
-      ],
+        );
+      },
     );
   }
 }
